f98a2db027c32eac371404e49dde4fdd204692d2
mistrzegiptu <mistrzegiptu@gmail.com>
2026-03-08 14:47:09 +0100

    First lab

---
+++ lab1/JavaUdpServer.java
@@ -0,0 +1,45 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Arrays;
+
+public class JavaUdpServer {
+
+    public static void main(String args[])
+    {
+        System.out.println("JAVA UDP SERVER");
+        DatagramSocket socket = null;
+        int portNumber = 9008;
+
+        try{
+            socket = new DatagramSocket(portNumber);
+            byte[] receiveBuffer = new byte[1024];
+
+
+            byte[] sendBuffer = "Pong Java Udp".getBytes();
+
+            while(true) {
+                Arrays.fill(receiveBuffer, (byte)0);
+                DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
+                socket.receive(receivePacket);
+                String msg = new String(receivePacket.getData());
+                System.out.println("received msg: " + msg);
+
+                InetAddress address = receivePacket.getAddress();
+                int clientPort = receivePacket.getPort();
+                System.out.println(address);
+                System.out.println(clientPort);
+                DatagramPacket sendPacket = new DatagramPacket(sendBuffer, sendBuffer.length, address, clientPort);
+                socket.send(sendPacket);
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        finally {
+            if (socket != null) {
+                socket.close();
+            }
+        }
+    }
+}
