6a86347881f218e35f3d875cebc5ee0e43921394
Mistrz Egiptu <mistrzegiptu@gmail.com>
2026-03-09 00:18:13 +0100

    Started working on chat

---
+++ SocketChat/Client/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Configuration;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Console.Write("Input your nickname: ");
+var nickname = Console.ReadLine();
+
+if (!IPAddress.TryParse(ConfigurationManager.AppSettings["ServerIP"], out var ipAddress))
+    throw new ConfigurationErrorsException();
+
+if(!int.TryParse(ConfigurationManager.AppSettings["ServerPort"], out var port))
+    throw new ConfigurationErrorsException();
+
+IPEndPoint ipEndPoint = new(ipAddress, port);
+
+using Socket client = new(ipEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+client.Connect(ipEndPoint);
+
+try
+{
+    while (true)
+    {
+        Console.Write("Enter message (EXIT to leave): ");
+        var message = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(message) || message == "EXIT")
+            break;
+
+        var messageBytes = Encoding.UTF8.GetBytes(message);
+
+        client.Send(messageBytes, SocketFlags.None);
+    }
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.ToString());
+}
+finally
+{
+    client.Shutdown(SocketShutdown.Both);
+}
---
+++ SocketChat/Server/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Configuration;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+if (!IPAddress.TryParse(ConfigurationManager.AppSettings["ServerIP"], out var ipAddress))
+    throw new ConfigurationErrorsException();
+
+if (!int.TryParse(ConfigurationManager.AppSettings["ServerPort"], out var port))
+    throw new ConfigurationErrorsException();
+
+IPEndPoint ipEndPoint = new(ipAddress, port);
+
+using Socket listener = new(ipEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+listener.Bind(ipEndPoint);
+listener.Listen();
+
+var handler = listener.Accept();
+while(true)
+{
+    var buffer = new byte[1024];
+    var received = handler.Receive(buffer, SocketFlags.None);
+    var response = Encoding.UTF8.GetString(buffer, 0, received);
+
+    Console.WriteLine(response);
+    Console.WriteLine(handler?.RemoteEndPoint?.ToString());
+}
